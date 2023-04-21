3dab01203091685f39a72a87f7e38edfc77b0091
Andrew O'Connor <andrew.oconnor@gmail.com>
2023-04-21 20:25:39 +0100

    Add some cube buildings and more bouncing balls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
+use rand::Rng;
 
 fn main() {
     App::new()
@@ -8,38 +9,84 @@ fn main() {
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_graphics)
         .add_startup_system(setup_physics)
-        .add_system(print_ball_altitude)
+        .add_startup_system(setup_buildings)
         .run();
 }
 
 fn setup_graphics(mut commands: Commands) {
     // Add a camera so we can see the debug-render.
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 20.0, 50.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
 }
 
-fn setup_physics(mut commands: Commands) {
+fn setup_buildings(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    for row in -5..5 {
+        for col in -5..5 {
+            let pos = Vec3::new(col as f32 * 10.0, 0.0, row as f32 * 10.0);
+
+            commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                    transform: Transform::from_translation(pos).with_scale(Vec3::new(5., 5., 5.)),
+                    ..default()
+                })
+                .insert(RigidBody::Fixed)
+                .insert(Collider::cuboid(0.5, 0.5, 0.5));
+        }
+    }
+}
+
+fn setup_physics(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     /* Create the ground. */
     commands
         .spawn(Collider::cuboid(100.0, 0.1, 100.0))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
 
     /* Create the bouncing ball. */
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(0.5))
-        .insert(Restitution::coefficient(0.7))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)))
-        .insert(Velocity {
-            linvel: Vec3::new(0.0, 0.0, -5.0),
-            angvel: Vec3::new(0.0, 0.0, 0.0),
-        });
-}
 
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..20 {
+        let pos = Vec3::new(
+            rng.gen_range(-50..=50) as f32,
+            50.,
+            rng.gen_range(-50..=50) as f32,
+        );
+
+        let vel = Vec3::new(
+            rng.gen_range(-10..=10) as f32,
+            0.,
+            rng.gen_range(-10..=10) as f32,
+        );
+
+        commands
+            .spawn(RigidBody::Dynamic)
+            .insert(Collider::ball(0.5))
+            .insert(Restitution::coefficient(0.95))
+            .insert(Velocity {
+                linvel: vel,
+                angvel: Vec3::new(0.0, 0.0, 0.0),
+            })
+            .insert(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::UVSphere {
+                    radius: 0.5,
+                    ..default()
+                })),
+                material: materials.add(Color::rgb(0.8, 0.1, 0.1).into()),
+                // transform: Transform::from_translation(pos).with_scale(Vec3::new(5., 5., 5.)),
+                ..default()
+            })
+            .insert(TransformBundle::from(Transform::from_translation(pos)));
     }
 }
