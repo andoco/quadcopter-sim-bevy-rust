b54c9fcc56f2adbbb53dd005a7e2da2b5c2fd47e
Andrew O'Connor <andrew.oconnor@gmail.com>
2023-04-23 13:31:04 +0100

    Snap external force to zero when low

--- src/main.rs
+++ src/main.rs
@@ -174,8 +174,16 @@ fn turn(
     ext_force.force *= 1.0 - time.delta_seconds();
     ext_force.torque *= 1.0 - time.delta_seconds();
 
+    if ext_force.force.length() < 0.1 {
+        ext_force.force = Vec3::ZERO;
+    }
+    if ext_force.torque.length() < 0.1 {
+        ext_force.torque = Vec3::ZERO;
+    }
+
     let turn_rate = 15_f32.to_radians();
-    let accel_rate = 100_f32;
+    let accel_rate = 10_f32;
+    let thrust_rate = 10_f32;
 
     if action_state.pressed(Action::Left) {
         ext_force.torque += Vec3::Y * turn_rate * time.delta_seconds();
@@ -190,6 +198,6 @@ fn turn(
         ext_force.force += tx.forward() * accel_rate * time.delta_seconds();
     }
     if action_state.pressed(Action::Thrust) {
-        ext_force.force += tx.up() * 50. * time.delta_seconds();
+        ext_force.force += tx.up() * thrust_rate * time.delta_seconds();
     }
 }
